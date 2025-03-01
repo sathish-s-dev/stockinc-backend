5a3a9ee881fe05443fa082f882a77edce6281c9e
sathish s <sathishs.webdev@gmail.com>
2025-03-01 23:16:13 +0530

    fix: middleware removed from index

--- src/index.ts
+++ src/index.ts
@@ -45,13 +45,6 @@ app.use(limiter);
 // middlware for checking API key
 app.use(checkApiKey);
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
-
 // Routes
 app.use("/stocks", stockRouter);
 app.use("/watchlist", watchlistRouter);
