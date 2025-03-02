72f05d63063452191112274575c7718dd3d13a56
sathish s <sathishs.webdev@gmail.com>
2025-03-02 15:57:42 +0530

    fix: auth controller removed

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,15 +1,1 @@
-import { Request, Response } from "express";
-import { errorResponse, successResponse } from "../utils/response";
-import data = require("../../db.json");
 
-const login = (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  const user = data.users.find(
-    (user) => user.email === email && user.password === password
-  );
-  if (user) {
-    res.json(successResponse("Login successful", user));
-  } else {
-    res.json(errorResponse("Invalid email or password"));
-  }
-};
